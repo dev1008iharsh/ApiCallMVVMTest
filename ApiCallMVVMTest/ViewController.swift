7d7eadc7a96d05f6a4a2f2917b4f6ec10603c7db
iHarsh Darji <dev.iharsh1008@gmail.com>
2026-04-09 17:13:37 +0530

    Initial Commit

---
+++ ApiCallMVVMTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiCallMVVMTest
+//
+//  Created by Harsh on 09/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
